3bdd2ce01279af868afd50f8312b06f51b7307a8
EllaC1234 <ellacaink@gmail.com>
2024-11-26 12:34:00 +0000

    Ruby data types notes

--- Ruby/data-types.rb
+++ Ruby/data-types.rb
@@ -13,6 +13,9 @@ b = a # Points to original address
 a = 'xxx' # Creates new value, and therefore new address
 puts b # Outputs 'HI'
 
+# There is no "boolean" type, but FalseClass and TrueClass
+# The only falsey values are false and nil. 
+
 # Arrays
 # When creating an array, you can use the following syntax to specify length etc before assigning items
 arr = Array.new(5, [])
@@ -20,3 +23,22 @@ arr = Array.new(5, [])
 arr = Array.new(10) { Array.new(10) }
 # Or...
 arr = [[1], [1,2]]
+
+# Symbols
+# Symbols are similar to strings, but are immutable, and therefore more memory efficient.
+status = :classified
+
+# Hashes
+# Hashes can be used to pass in multiple options in a user-friendly way
+def total_weight(options={}) # Options are set to a default of nil
+  a = options[:soccer_ball_count] || 0 # Will set a to 0 if options[...] is false
+  b = options[:tennis_ball_count] || 0
+  c = options[:golf_ball_count] || 0
+
+  (a * 410) + (b * 58) + (c * 45) + 29
+end
+
+x = total_weight()
+y = total_weight(soccer_ball_count: 3, tennis_ball_count: 2, golf_ball_count: 1)
+puts x
+puts y
